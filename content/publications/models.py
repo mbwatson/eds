d5f3f65c7abbf7a156b1aed43617bb03c34ab2db
mbwatson <mattwat@gmail.com>
2018-07-10 15:18:59 -0400

    publication-project relationship

--- content/publications/models.py
+++ content/publications/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from content.staff.models import StaffMember
-from content.projects.models import Project
 
 class Publication(models.Model):
     title = models.CharField(max_length=200, blank=False)
@@ -8,7 +7,6 @@ class Publication(models.Model):
     link = models.CharField(max_length=200, blank=True)
     citation = models.TextField(blank=True)
     staff = models.ManyToManyField(StaffMember, blank=True)
-    projects = models.ManyToManyField(Project, blank=True)
 
     def __str__(self):
         return self.title
