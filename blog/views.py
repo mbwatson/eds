a746746d2f57e240861af7d8503eb728b0540eca
mbwatson <mattwat@gmail.com>
2018-07-10 18:47:45 -0400

    load posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import get_object_or_404, render
+from .models import Category, Post
 
 def index(request):
+    posts = get_list_or_404(Post)
     context = {
         'page_title': 'Blog',
+        'posts': posts
     }
     return render(request, 'blog/index.html', context)
 
+def detail(request, post_id):
+    context = {
+        'page_title': 'Blog Post',
+    }
+    return render(request, 'blog/detail.html', context)
