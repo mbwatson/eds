3d71b4e05d351376d0e194c2b02eaef23d86ccb9
mbwatson <mattwat@gmail.com>
2018-07-10 18:40:46 -0400

    path to blog

--- eds/urls.py
+++ eds/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('projects/', include('content.projects.urls')),
     path('publications/', include('content.publications.urls')),
     path('datasets/', include('content.datasets.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
