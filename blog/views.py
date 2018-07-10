0ed6f74711bbb261a469a6b80f9b42e5274775c9
mbwatson <mattwat@gmail.com>
2018-07-10 18:41:33 -0400

    init views file

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+def index(request):
+    context = {
+        'page_title': 'Blog',
+    }
+    return render(request, 'blog/index.html', context)
+
