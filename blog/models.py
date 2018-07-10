2ba8cf0f71402598355493361cfb2222bfc6c875
mbwatson <mattwat@gmail.com>
2018-07-10 17:47:10 -0400

    add blog app and create models

---
+++ blog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+class Category(models.Model):
+    title = models.CharField(max_length=255,null=False)
+    description = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_published = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('Draft', 'Draft'),
+        ('Published', 'Published'),
+    )
+    title = models.CharField(max_length=255,null=False)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Draft')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_published = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
