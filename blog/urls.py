07d4f287d856364fdc27020cdc9ea56329eec57f
mbwatson <mattwat@gmail.com>
2018-07-11 09:48:31 -0400

    route to posts by category

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<slug:post_slug>', views.detail, name='detail'),
+    path('<slug:category_slug>', views.category, name='category'),
 ]
