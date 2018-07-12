bd36b9864b474a6db177b8ff03fa566f821059cb
mbwatson <mattwat@gmail.com>
2018-07-12 10:37:52 -0400

    reference tinymce

--- eds/settings.py
+++ eds/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'content',
     'blog',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,32 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'content/media')
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static-assets'), )
+
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 1120,
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    # 'theme': 'modern',
+    # 'plugins': '''
+    #         textcolor save link image media preview codesample contextmenu
+    #         table code lists fullscreen  insertdatetime  nonbreaking
+    #         contextmenu directionality searchreplace wordcount visualblocks
+    #         visualchars code fullscreen autolink lists  charmap print  hr
+    #         anchor pagebreak
+    #         ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+    }
