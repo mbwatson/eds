8db9941fa55511aff157aedd5261386ae3c66283
mbwatson <mattwat@gmail.com>
2018-07-12 14:07:11 -0400

    add category links & styling on blog list

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ def index(request):
     posts = get_list_or_404(Post)
     categories = get_list_or_404(Category)
     context = {
-        'page_title': 'Blog',
         'categories': categories,
         'posts': posts,
     }
@@ -14,17 +13,17 @@ def index(request):
 def detail(request, post_slug):
     post = get_object_or_404(Post, slug=post_slug)
     context = {
-        'page_title': post.title,
         'post': post,
     }
     return render(request, 'blog/detail.html', context)
 
 def category(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
+    categories = get_list_or_404(Category)
     posts = get_list_or_404(Post, category=category.pk)
     context = {
-        'page_title': category.title,
         'category': category,
+        'categories': categories,
         'posts': posts,
     }
     return render(request, 'blog/category.html', context)
