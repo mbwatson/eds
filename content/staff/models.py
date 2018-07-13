3b1e6d073a8a37aa7787dae8fa025fb5ac0fef9e
mbwatson <mattwat@gmail.com>
2018-07-13 09:56:18 -0400

    add slug automatically-generated and non-editable slug  field

--- content/staff/models.py
+++ content/staff/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class StaffMember(models.Model):
     first_name = models.CharField(max_length=63, blank=False)
     last_name = models.CharField(max_length=63, blank=False)
     job_title = models.CharField(max_length=127, blank=False)
+    slug = models.SlugField(max_length=255, editable=False, unique=True)
     bio = models.TextField(blank=True)
     photo = models.ImageField(upload_to='staff_pics', blank=True)
 
@@ -12,6 +14,10 @@ class StaffMember(models.Model):
 
     def display_name(self):
         return f'{self.first_name} {self.last_name}'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(f'{self.first_name} {self.last_name}')
+        super(StaffMember, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ('last_name', 'first_name')
